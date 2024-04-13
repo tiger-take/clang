5be2583d823722962217b2500cc9a8d7820da395
tiger-takechan <1642204+tiger-take@users.noreply.github.com>
2024-04-13 21:38:38 +0900

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,1 +1,14 @@
+#include "pxt.h"
 
+namespace helloworld {
+    
+    //%
+    void showAsyncString(String text) {
+        uBit.display.scrollAsync(MSTR(text));
+    }
+
+    //%
+    void stopAnimation() {
+        uBit.display.stopAnimation();
+    }
+}
