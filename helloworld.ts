9376d5a85af4334c5855dab3566e83e866a7c22f
tiger-takechan <1642204+tiger-take@users.noreply.github.com>
2024-04-13 21:38:18 +0900

    Update helloworld.ts

--- helloworld.ts
+++ helloworld.ts
@@ -1,1 +1,30 @@
 // ここにコードを追加します
+/**
+ * Hello World blocks
+ * icon: a Unicode identifier for an icon from the Font Awesome icon set.
+ *       http://fontawesome.io/icons
+ */
+//% weight=100 color=#696969 icon="\uf1b2"
+namespace helloworld {
+
+    /**
+     * Show string, async
+     * @param text 非同期でスクロール表示する文字列
+     */
+    //% block
+    //% shim=helloworld::showAsyncString
+    export function showAsyncString(text: string): void {
+        // for the simulator
+        basic.showString(text)
+    }
+
+    /**
+     * Stop animation
+     */
+    //% block
+    //% shim=helloworld::stopAnimation
+    export function stopAnimation(): void {
+        // for the simulator
+        basic.clearScreen()
+    }
+}
