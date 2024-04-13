0ce8fc22e84ecadf4f03fcc04b206af698186bc9
tiger-takechan <1642204+tiger-take@users.noreply.github.com>
2024-04-13 21:55:36 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    reset()
+    helloworld.showAsyncString("Happy birthday!")
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Birthday), music.PlaybackMode.InBackground)
 })
+input.onButtonPressed(Button.B, function () {
+    reset()
+    basic.showString("Hello!")
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+})
+function reset () {
+    helloworld.stopAnimation()
+    basic.clearScreen()
+    music.stopAllSounds()
+    basic.pause(100)
+}
+basic.showIcon(IconNames.Heart)
